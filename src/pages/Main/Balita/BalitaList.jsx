23092acfae3ff7386d795fec9ecd3b28be48fba6
Rafqiagata <rafqiagatha@gmail.com>
2020-08-15 22:03:27 +0700

    Nambah list balita

---
+++ src/pages/Main/Balita/BalitaList.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { List } from "antd/lib/form/Form";
+import Avatar from "antd/lib/avatar/avatar";
+
+export const BalitaList = ({ balitaList }) => {
+  const data = [
+    {
+      title: "Ant Design Title 1",
+    },
+    {
+      title: "Ant Design Title 2",
+    },
+    {
+      title: "Ant Design Title 3",
+    },
+    {
+      title: "Ant Design Title 4",
+    },
+  ];
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title={<a href="https://ant.design">{item.title}</a>}
+            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
+  );
+};
