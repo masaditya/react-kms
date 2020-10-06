da32c96ba12bbaf67e2980812e95e6350f0b431f
Bawazir Fadhil Mohammad <bawazirfadhil@gmail.com>
2020-10-06 11:55:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// yokpo tod
 // hehe tod
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
